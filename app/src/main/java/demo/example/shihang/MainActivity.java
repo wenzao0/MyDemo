55c0013e5bb30af9caa98106dd12770cbc9ddf70
e <905249148@qq.com>
2017-01-06 16:21:05 +0800

    增加test方法

--- app/src/main/java/demo/example/shihang/MainActivity.java
+++ app/src/main/java/demo/example/shihang/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,8 +25,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                test();
             }
         });
+    }
+
+    private void test(){
+        Log.d("****","123");
     }
 
     @Override
